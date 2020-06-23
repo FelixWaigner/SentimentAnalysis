16e3f8bd87fe0399f3a8ae07723b5d8c6c532d3f
Felix-yz <Waigner.Felix@t-online.de>
2020-06-23 17:09:57 +0200

    added sentiment analysis and piechart visualipie chart visualization

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ consumerSecret = CONSUMER_SECRET
 accessToken = ACCESS_TOKEN
 accessTokenSecret = ACCESS_TOKEN_SECRET
 
-print(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+#print(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 auth = tweepy.OAuthHandler(consumer_key=consumerKey, consumer_secret=consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
@@ -28,7 +28,7 @@ noOfSearchTerms = int(input("Enter how many tweets to analyze: "))
 
 
 
-tweets = tweepy.Cursor(api.search, q=searchTerm, lang="de").items(noOfSearchTerms)
+tweets = tweepy.Cursor(api.search, q=searchTerm, lang="en").items(noOfSearchTerms)
 
 positive = 0
 negative = 0
@@ -37,4 +37,40 @@ polarity = 0
 
 for tweet in tweets:
     print(tweet.text)
+    print("")
     analysis = TextBlob(tweet.text)
+    polarity += analysis.sentiment.polarity
+
+    if(analysis.sentiment.polarity == 0):
+        neutral += 1
+    elif(analysis.sentiment.polarity < 0.00):
+        negative += 1
+    elif(analysis.sentiment.polarity > 0.00):
+        positive += 1
+
+positive = percentage(positive, noOfSearchTerms)
+negative = percentage(negative, noOfSearchTerms)
+neutral = percentage(neutral, noOfSearchTerms)
+
+positive = format(positive, ".2f")
+neutral = format(neutral, ".2f")
+negative = format(negative, ".2f")
+
+print("How peaple are reacting on " + searchTerm + " by analyzing " + str(noOfSearchTerms) + " Tweets.")
+
+if(polarity == 0):
+    print("Neutral")
+if(polarity < 0):
+    print("Negative")
+if(polarity > 0):
+    print("Positive")
+
+labels = ["Positive ["+str(positive)+"%]", "Neutral ["+str(neutral)+"%]", "Negative ["+str(negative)+"%]"]
+sizes = [positive, neutral, negative]
+colors = ["yellowgreen", "gold", "red"]
+patches, texts = plt.pie(sizes, colors=colors, startangle=90)
+plt.legend(patches, labels, loc="best")
+plt.title(("How peaple are reacting on " + searchTerm + " by analyzing " + str(noOfSearchTerms) + " Tweets."))
+plt.axis("equal")
+plt.tight_layout()
+plt.show()

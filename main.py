116d5f9637386ebdb4ce5cb797fca65992cadca3
Felix-yz <Waigner.Felix@t-online.de>
2020-06-24 20:38:47 +0200

    Added Excel export to store Dataset

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from textblob import TextBlob
 import sys, tweepy
 import matplotlib.pyplot as plt
+from openpyxl import *
 
 from twitterAPIKeys import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
+filePath = "C:/Users/Waign/Desktop/Sentiment.xlsx"
+wb = load_workbook(filePath)
+ws = wb["Tabelle1"]
 
 def percentage(part, whole):
     return 100 * float(part)/float(whole)
@@ -23,22 +27,27 @@ api = tweepy.API(auth)
 
 print("Connected to Twitter API")
 
-searchTerm = input("Enter keyword/hashtag to search about: ")
-noOfSearchTerms = int(input("Enter how many tweets to analyze: "))
+#searchTerm = input("Enter keyword/hashtag to search about: ")
+#noOfSearchTerms = int(input("Enter how many tweets to analyze: "))
+
+searchTerm = "Warzone"
+noOfSearchTerms = 500
 
 
 
-tweets = tweepy.Cursor(api.search, q=searchTerm, lang="en").items(noOfSearchTerms)
+tweets = tweepy.Cursor(api.search, q=searchTerm, lang="en", tweet_mode="extended").items(noOfSearchTerms)
 
 positive = 0
 negative = 0
 neutral = 0
 polarity = 0
 
+x = 1
+
 for tweet in tweets:
-    print(tweet.text)
-    print("")
-    analysis = TextBlob(tweet.text)
+    #print(tweet.full_text)
+    #print("")
+    analysis = TextBlob(tweet.full_text)
     polarity += analysis.sentiment.polarity
 
     if(analysis.sentiment.polarity == 0):
@@ -47,6 +56,16 @@ for tweet in tweets:
         negative += 1
     elif(analysis.sentiment.polarity > 0.00):
         positive += 1
+
+
+    wcell1 = ws.cell(x, 1)
+    wcell2 = ws.cell(x, 2)
+    wcell1.value = tweet.full_text
+    wcell2.value = analysis.sentiment.polarity
+    x += 1
+
+
+wb.save(filePath)
 
 positive = percentage(positive, noOfSearchTerms)
 negative = percentage(negative, noOfSearchTerms)
